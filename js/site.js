e386c79abb1e835c874d665128c03e1ff4f7f0a0
EntityBug <FMN_TZX@outlook.com>
2021-06-18 19:58:27 +0800

    test1

---
+++ js/site.js
@@ -0,0 +1,11 @@
+﻿function getText() {
+    var url = "http://entitybugmcserver.free.idcfengye.com/docs/Notification.txt";
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onreadystatechange = function () {
+        console.log(request.status)
+        if (request.readyState !== 4) return;
+        document.append(request.responseText)
+    }
+    request.send(null);
+}
